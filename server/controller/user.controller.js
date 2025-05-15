6641b416e466ca2cb5bec1146e25598b5e614c98
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2025-05-15 17:49:08 +0530

    user controller register

---
+++ server/controller/user.controller.js
@@ -0,0 +1,116 @@
+import sendEmail from '../config/sendEmail.js'
+import UserModel from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
+
+
+
+export async function registerUserController(request,response){
+    try {
+        const { name, email , password } = request.body
+
+        if(!name || !email || !password){
+            return response.status(400).json({
+                message : "provide email, name, password",
+                error : true,
+                success : false
+            })
+        }
+
+        const user = await UserModel.findOne({ email })
+
+        if(user){
+            return response.json({
+                message : "Already register email",
+                error : true,
+                success : false
+            })
+        }
+
+        const salt = await bcryptjs.genSalt(10)
+        const hashPassword = await bcryptjs.hash(password,salt)
+
+        const payload = {
+            name,
+            email,
+            password : hashPassword
+        }
+
+        const newUser = new UserModel(payload)
+        const save = await newUser.save()
+
+
+
+        return response.json({
+            message : "User register successfully",
+            error : false,
+            success : true,
+            data : save
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+
+
+export async function loginController(request,response){
+    try {
+        const { email , password } = request.body
+
+
+        if(!email || !password){
+            return response.status(400).json({
+                message : "provide email, password",
+                error : true,
+                success : false
+            })
+        }
+
+        const user = await UserModel.findOne({ email })
+
+        if(!user){
+            return response.status(400).json({
+                message : "User not register",
+                error : true,
+                success : false
+            })
+        }
+
+        if(user.status !== "Active"){
+            return response.status(400).json({
+                message : "Contact to Admin",
+                error : true,
+                success : false
+            })
+        }
+
+        const checkPassword = await bcryptjs.compare(password,user.password)
+
+        if(!checkPassword){
+            return response.status(400).json({
+                message : "Check your password",
+                error : true,
+                success : false
+            })
+        }
+
+        return response.json({
+            message : "Login successfully",
+            error : false,
+            success : true,
+          
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
